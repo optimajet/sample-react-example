49ac890d80c76339d04e10e9bfe5dc90268abfc5
Evgeny Betin <betin.evgeniy@gmail.com>
2023-03-15 17:08:46 +0300

    set state, set activity, resume

--- frontend/src/ProcessMenuChangeState.js
+++ frontend/src/ProcessMenuChangeState.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useMemo, useState} from "react";
-import {Button, InputPicker, Modal} from "rsuite";
+import {useEffect, useState} from "react";
+import {Button, InputPicker, Message, Modal, useToaster} from "rsuite";
 import ProcessParameters from "./ProcessParameters";
 import settings from "./settings";
 import { startCase } from 'lodash'
@@ -8,69 +8,72 @@ const actions = ['SetState', 'SetActivity', 'Resume'].map(
   item => ({ label: startCase(item), value: item })
 );
 
-const ProcessMenuChangeState = ({open, onClose, processId, currentUser, ...props}) => {
+const ProcessMenuChangeState = ({open, onClose, processId, currentUser}) => {
 
     const [selectedAction, setSelectedAction] = useState('SetState')
     const [newProcessParameters, setNewProcessParameters] = useState([])
-    const [states, setStates] = useState({
+    const initialData = {
         states: [],
-        currentState: ''
-    })
-    const [activities, setActivities] = useState({
+        currentState: '',
         activities: [],
-        currentActivity: ''
-    })
-    const [processParameters, setProcessParameters] = useState([])
-    const loadStates = (processId) => {
-        fetch(`${settings.workflowUrl}/states/${processId}/`)
+        currentActivity: '',
+        processParameters: [],
+        loaded: false
+    };
+    const [data, setData] = useState(initialData)
+    const toaster = useToaster();
+
+    const loadData = (processId) => {
+        const fetches = []
+        const data = {}
+        const statesRequest = fetch(`${settings.workflowUrl}/states/${processId}/`)
             .then(result => result.json())
             .then(result => {
-                setStates({
+                Object.assign(data, {
                     states: result.map(item => ({label: item.localizedName, value: item.name})),
-                    currentState: result.find(item => !!item.isCurrent)?.name
+                    currentState: result.find(item => !!item.isCurrent)?.name ?? result[0]?.name
                 })
+                return result
             })
-    }
-
-    useEffect(() => {
-        if (open) {
-            loadStates(processId);
-        }
-    }, [processId, open]);
-
-    const loadActivities = (processId) => {
-        fetch(`${settings.workflowUrl}/activities/${processId}/`)
+        fetches.push(statesRequest)
+        const activitiesRequest = fetch(`${settings.workflowUrl}/activities/${processId}/`)
             .then(result => result.json())
             .then(result => {
-                setActivities({
+                Object.assign(data, {
                     activities: result.map(item => ({label: item.name, value: item.name})),
                     currentActivity: result.find(item => !!item.isCurrent)?.name
                 })
+                return result
             })
-    }
-
-    useEffect(() => {
-        if (open) {
-            loadActivities(processId);
-        }
-    }, [processId, open]);
-
-    const loadParameters = (processId) => {
-        fetch(`${settings.workflowUrl}/schemeParameters/${processId}/`)
+        fetches.push(activitiesRequest)
+        const parametersRequest = fetch(`${settings.workflowUrl}/schemeParameters/${processId}/`)
             .then(result => result.json())
             .then(result => {
-               setProcessParameters(result)
+                Object.assign(data, {
+                    ...data,
+                    processParameters: result
+                })
+                return result
             })
+        fetches.push(parametersRequest)
+
+        Promise.all(fetches).then(_ => {
+              Object.assign(data, {
+                  loaded: true
+              });
+            setData(data)
+        })
     }
 
     useEffect(() => {
         if (open) {
-            loadParameters(processId);
+            loadData(processId);
+        } else {
+            setData(initialData)
         }
     }, [processId, open]);
 
-     const execute = () => {
-
+    const execute = () => {
         fetch(`${settings.workflowUrl}/${selectedAction}/${processId}/${getSelectedStateOrActivity()}/${currentUser}/`,
             {
                 method: "post",
@@ -82,40 +85,49 @@ const ProcessMenuChangeState = ({open, onClose, processId, currentUser, ...props
                     processParameters: newProcessParameters
                 })
             })
-            .then(result => result.json())
+            .then(result => {
+                if (result.status !== 200) {
+                    throw new Error(result.statusText)
+                }
+                return result.json()})
             .then(() => {
                 onClose(true)
-            });
+            })
+            .catch((error) => {
+                const message = <Message type="error" closable={true}> {error.toString()} </Message>
+                toaster.push(message, {placement : 'topCenter', duration: 20000})
+                onClose(true)
+            })
     }
 
     const getSelectedStateOrActivity = () => {
-        return  selectedAction === 'SetState' ? states.currentState : activities.currentActivity;
+        return selectedAction === 'SetState' ? data.currentState : data.currentActivity;
     }
 
-    return <Modal open={open} onClose={onClose} overflow={true}>
+    return <Modal open={open && data.loaded} onClose={onClose} overflow={true}>
         <Modal.Header>
             <Modal.Title>Select your action</Modal.Title>
         </Modal.Header>
         <Modal.Body>
             <InputPicker data={actions} value={selectedAction} onChange={(value) => setSelectedAction(value)}/>
             {selectedAction === 'SetState' &&
-                <InputPicker data={states.states} value={states.currentState}
-                             onChange={(value) => setStates({...states, currentState: value})}/>
+                <InputPicker data={data.states} value={data.currentState}
+                             onChange={(value) => setData({...data, currentState: value})}/>
             }
             {selectedAction !== 'SetState' &&
-                <InputPicker data={activities.activities} value={activities.currentActivity}
+                <InputPicker data={data.activities} value={data.currentActivity}
                              onChange={(value) => {
-                                 setActivities({...activities, currentActivity: value})
+                                 setData({...data, currentActivity: value})
                              }}/>
             }
             <ProcessParameters onParametersChanged={(processParameters) => setNewProcessParameters(processParameters)}
-                               defaultParameters={processParameters}/>
+                               defaultParameters={data.processParameters}/>
         </Modal.Body>
         <Modal.Footer>
             <Button onClick={execute} appearance="primary">
                 {`${startCase(selectedAction)} ${selectedAction === 'Resume' ? 'from' : ''} ${getSelectedStateOrActivity()}`}
             </Button>
-            <Button onClick={onClose} appearance="subtle">
+            <Button onClick={() => onClose(false) } appearance="subtle">
                 Cancel
             </Button>
         </Modal.Footer>
