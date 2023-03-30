f0ba2dca941666c14ac805add9ac672200258651
betine <betin.evgeniy@gmail.com>
2023-03-30 13:59:39 +0300

    React sample in Docker
    React sample in Docker

---
+++ Backend/WorkflowApi/WorkflowApiConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace WorkflowApi;
+
+public class WorkflowApiConfiguration
+{
+    public WorkflowApiCorsConfiguration Cors { get; set; }
+
+    public string LicenseKey { get; set; }
+}
+
+public class WorkflowApiCorsConfiguration
+{
+    public List<string> Origins { get; set; }
+}
