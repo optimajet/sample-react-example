a25a9ee483acc7f3daf2b9ff09509605bebc985f
Aleksandr Shalugin <shalugin.alexandr@gmail.com>
2023-03-31 15:41:15 +0300

    Added keys and fixed HTML

--- frontend/src/ProcessConsole.js
+++ frontend/src/ProcessConsole.js
@@ -6,11 +6,11 @@ import ReactMarkdown from 'react-markdown';
 const ProcessConsoleTimeline = ({processId, align = 'left'}) => {
     const processesConsoleContext = useProcessesConsoleContext();
     const consoleLines = processesConsoleContext[processId] ?? [];
-    const timeLineItems = consoleLines.map((ti, i) => <Timeline.Item
+    const timeLineItems = consoleLines.map((ti, i) => <Timeline.Item key={i}
         dot={i === 0 ? <CircleIcon style={{color: '#15b215'}}/> : <CircleIcon/>}>
         <p>{`${ti.date.toLocaleDateString()} ${ti.date.toLocaleTimeString()}`}</p>
-        {ti.message.split('\n').map(s => {
-            return <p><ReactMarkdown>{s}</ReactMarkdown></p>
+        {ti.message.split('\n').map((s, index) => {
+            return <div key={index}><ReactMarkdown>{s}</ReactMarkdown></div>
         })}
     </Timeline.Item>)
     return <Timeline align={align}>{timeLineItems}</Timeline>
